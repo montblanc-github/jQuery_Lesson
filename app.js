5d00903500abc4703a17a2d4b2f05bffbba1c053
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-13 20:35:47 +0900

    [Add] スライドする動きをつける

--- app.js
+++ app.js
@@ -6,9 +6,23 @@
 // });
 
 // 5章学習内容
-$(function () {
-  $(".box1").css({
-    "background-color": "#0000FF",
-    height: "100px",
-  });
-});
+// cssの内容を変更
+// $(function () {
+//   $(".box1").css({
+//     "background-color": "#0000FF",
+//     height: "100px",
+//   });
+// });
+
+// 動きをつける
+// 上からしたへスライド→cssにdisplay:noneを追加
+// $(function () {
+//   $(".box1").slideDown();
+// });
+
+// 下から上へスライド→cssのdisplay:noneは不要
+// $(function () {
+//   $(".box1").slideUp();
+// });
+
+// 
