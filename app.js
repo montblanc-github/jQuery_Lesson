edb61b268df0ffc40630a3b756d326cfc0738aa6
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-13 20:30:43 +0900

    [Start] JavaScript5章

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+// 4章学習内容
 // jQueryの基本書式＝$( セレクタ ).メソッド ( パラメータ );
-$(document).ready(function () {
-  // 意味：bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
-  $("body").html("<h1>Hello jQuery!!</h1>");
+// $(document).ready(function () {
+//   // 意味：bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
+//   $("body").html("<h1>Hello jQuery!!</h1>");
+// });
+
+// 5章学習内容
+$(function () {
+  $(".box1").css({
+    "background-color": "#0000FF",
+    height: "100px",
+  });
 });
