3eb2216f507d8793ec11434be149ef035e46f807
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-18 10:51:06 +0900

    [Start] JavaScript6章

--- app.js
+++ app.js
@@ -37,14 +37,17 @@
 // });
 
 // 確認問題
-$(function () {
-  $(".box1").slideDown(function () {
-    $(".box1")
-      .css({
-        "background-color": "#0000FF",
-        width: "200px",
-        height: "100px",
-      })
-      .slideUp();
-  });
-});
+// $(function () {
+//   $(".box1").slideDown(function () {
+//     $(".box1")
+//       .css({
+//         "background-color": "#0000FF",
+//         width: "200px",
+//         height: "100px",
+//       })
+//       .slideUp();
+//   });
+// });
+
+// 6章学習内容
+
