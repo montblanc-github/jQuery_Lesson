8863f2a70142181bd7d35fe3a316b3aafb940518
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-18 10:57:34 +0900

    [Add] マウスオーバー/マウスアウトイベント追加

--- app.js
+++ app.js
@@ -50,4 +50,11 @@
 // });
 
 // 6章学習内容
-
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#FF0000'});
+  });  
+});
