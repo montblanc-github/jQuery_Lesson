49fc607ac8ed4890f968bfb3db9ef03256f6eafc
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-18 11:00:06 +0900

    [Add] イベントクラス追加

--- app.js
+++ app.js
@@ -52,9 +52,9 @@
 // 6章学習内容
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color':'#0000FF'});
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color':'#FF0000'});
-  });  
+    $('.box1').removeClass('box1-ext');
+  });
 });
